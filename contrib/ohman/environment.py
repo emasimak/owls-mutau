8f7b919bc3461591c510e0385fbbbaba268db8cc
Henrik Ohman <speeph@gmail.com>
2015-04-15 10:04:19 +0200

    Changed to setup without parallelization and cacheing
    Disabled parallelization. Disabled Cacheing. Removed reference to numexpr.

--- contrib/ohman/environment.py
+++ contrib/ohman/environment.py
@@ -1,6 +1,3 @@
-# numexpr imports
-import numexpr as ne
-
 # owls-cache imports
 from owls_cache.transient import set_cache_debug as set_transient_cache_debug
 from owls_cache.persistent import set_cache_debug as set_persistent_cache_debug
@@ -14,22 +11,20 @@ from owls_parallel.backends.multiprocessing import \
 # Make it clear that we're in this environment
 print('Using Ohman\'s environment...')
 
-# Set the maximum number of threads that a single evaulator process can use
-# NOTE: This needs to be done before creating the parallelization backend so
-# that the setting is fork()'d properly
-ne.set_num_threads(1)
-
 # Enable cache debugging
 # set_transient_cache_debug(True)
 # set_persistent_cache_debug(True)
 
 # Set the persistent cache
-persistent_cache = RedisPersistentCache(
-    unix_socket_path='/home/howard/.cache.sock'
-)
+#persistent_cache = RedisPersistentCache(
+    #unix_socket_path='/home/ohman/.cache.sock'
+#)
+
+# Disable the persistent cache
+persistent_cache = None
 
 # Create parallelization backend
-parallelization_backend = MultiprocessingParallelizationBackend(4)
+#parallelization_backend = MultiprocessingParallelizationBackend(24)
 
 # Disable parallelization
-# parallelization_backend = None
+parallelization_backend = None
