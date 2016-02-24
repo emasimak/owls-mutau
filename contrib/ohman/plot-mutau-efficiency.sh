0f9e54a9c3dc1e671e208a9a5e0a31df02cf8c47
Henrik Ohman <speeph@gmail.com>
2016-02-24 15:50:35 +0100

    Systematic variations for tau trigger efficiencies
    Added computation of systematic variations for tau trigger efficiencies.
    Added printing of counts and differences for systematic variations.

--- contrib/ohman/plot-mutau-efficiency.sh
+++ contrib/ohman/plot-mutau-efficiency.sh
@@ -4,28 +4,31 @@
 CONTRIB=$(dirname "$0")
 OWLS="$CONTRIB/../.."
 
-EXTENSIONS="pdf eps"
-#EXTENSIONS="pdf"
+#EXTENSIONS="pdf eps"
+EXTENSIONS="pdf"
 LUMINOSITY=3209.0 # 1/pb
 DATA_PREFIX="/disk/d1/ohman/tagprobe_2016-01-21_merged/"
 
 echo ">>> Plotting tau efficiency and scale factors"
 OUTPUT="results_mutau/plots_tau_efficiency"
 
-# Tau efficiency
-for REGION in mu_tau_gradient mu_tau_loose mu_tau_tight mu_tau_drcut mu_tau_mtcut mu_tau_ttbar_cr mu_tau_1b_cr mu_tau_loose_ttbar_cr
+# tau efficiency
+for REGION in mu_tau
+#for REGION in mu_tau mu_tau_ttbar_cr
 do
   OUTPUT="results_mutau/plots_tau_efficiency/$REGION"
   "$OWLS/tools/plot-tau-efficiency.py" \
     --output "$OUTPUT" \
     --extensions $EXTENSIONS \
-    --model-file "$OWLS/share/mutau/models.py" \
+    --model-file "$OWLS/share/mutau/models-2016-01-21.py" \
     --model osss_sub \
-    --regions-file "$OWLS/share/mutau/regions.py" \
+    --regions-file "$OWLS/share/mutau/regions-2016-01-21.py" \
     --region $REGION \
     --distributions-file "$OWLS/share/mutau/distributions.py" \
     --environment-file "$CONTRIB/environment.py" \
     --root-output \
+    --text-output \
     -- \
-    data_prefix=$DATA_PREFIX
+    data_prefix=$DATA_PREFIX \
+    enable_systematics=True
 done
--- tools/plot-tau-efficiency.py
+++ tools/plot-tau-efficiency.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#encoding: utf-8
 
 # System imports
 import argparse
@@ -20,13 +21,27 @@ from owls_parallel import ParallelizedEnvironment
 
 # owls-hep imports
 from owls_hep.module import load as load_module
-from owls_hep.utility import efficiency, add_histograms
+from owls_hep.utility import efficiency, add_histograms, integral
 from owls_hep.plotting import Plot, style_line
+from owls_hep.uncertainty import to_shape, sum_quadrature
 
 # owls-taunu imports
 from owls_taunu.variations import Filtered, OneProng, ThreeProng
 from owls_taunu.styling import default_black, default_red
 from owls_taunu.plotting import plot, plot2d
+from owls_taunu.mutau.uncertainties import TestSystFlat, TestSystShape, \
+        MuonEffStat, MuonEffSys, \
+        MuonEffTrigStat, MuonEffTrigSys, \
+        MuonIsoStat, MuonIsoSys, \
+        MuonIdSys, MuonMsSys, MuonScaleSys, \
+        RqcdStat, RqcdSyst, \
+        BJetEigenB0, BJetEigenB1, BJetEigenB2, BJetEigenB3, BJetEigenB4, \
+        BJetEigenC0, BJetEigenC1, BJetEigenC2, BJetEigenC3, \
+        BJetEigenLight0, BJetEigenLight1, BJetEigenLight2, BJetEigenLight3, \
+        BJetEigenLight4, BJetEigenLight5, BJetEigenLight6, BJetEigenLight7, \
+        BJetEigenLight8, BJetEigenLight9, BJetEigenLight10, BJetEigenLight11, \
+        BJetEigenLight12, BJetEigenLight13, \
+        BJetExtrapolation
 
 # ROOT imports
 from ROOT import TGraphAsymmErrors, TFile, SetOwnership
@@ -81,14 +96,11 @@ parser.add_argument('--root-output',
                     action = 'store_true',
                     help = 'write plotted histograms to a ROOT file',
                    )
-parser.add_argument('--scale-factors',
+parser.add_argument('-t',
+                    '--text-output',
                     action = 'store_true',
-                    help = 'plot scale factors',
-                   )
-parser.add_argument('--variations',
-                    action = 'store_true',
-                    help = 'do variations',
-                   )
+                    help = 'enable text output of counts for systematic '
+                    'variations')
 parser.add_argument('-x',
                     '--extensions',
                     nargs = '+',
@@ -146,31 +158,72 @@ efficiencies = OrderedDict({
         'distribution': distributions_file.tau_pt_trig_b1,
         'filter': triggered_tau25,
     },
-    'tau25_3p': {
-        'label': ['HLT_tau25_medium1_tracktwo'],
-        'region': three_prong,
-        'title': ' (3-prong)',
-        'distribution': distributions_file.tau_pt_trig_b1,
-        'filter': triggered_tau25,
-    },
+    #'tau25_3p': {
+        #'label': ['HLT_tau25_medium1_tracktwo'],
+        #'region': three_prong,
+        #'title': ' (3-prong)',
+        #'distribution': distributions_file.tau_pt_trig_b1,
+        #'filter': triggered_tau25,
+    #},
 
-    'tau25_noiso_1p': {
-        'label': ['HLT_tau25_medium1_tracktwo_L1TAU12'],
-        'region': one_prong,
-        'title': ' (1-prong)',
-        'distribution': distributions_file.tau_pt_trig_b1,
-        'filter': triggered_tau25_noiso,
-    },
-    'tau25_noiso_3p': {
-        'label': ['HLT_tau25_medium1_tracktwo_L1TAU12'],
-        'region': three_prong,
-        'title': ' (3-prong)',
-        'distribution': distributions_file.tau_pt_trig_b1,
-        'filter': triggered_tau25_noiso,
-    },
+    #'tau25_noiso_1p': {
+        #'label': ['HLT_tau25_medium1_tracktwo_L1TAU12'],
+        #'region': one_prong,
+        #'title': ' (1-prong)',
+        #'distribution': distributions_file.tau_pt_trig_b1,
+        #'filter': triggered_tau25_noiso,
+    #},
+    #'tau25_noiso_3p': {
+        #'label': ['HLT_tau25_medium1_tracktwo_L1TAU12'],
+        #'region': three_prong,
+        #'title': ' (3-prong)',
+        #'distribution': distributions_file.tau_pt_trig_b1,
+        #'filter': triggered_tau25_noiso,
+    #},
 
     # TODO: Add tau35, tau80, tau125
 })
+
+# NOTE: See model file for comments about pruning
+systematics = [
+    #TestSystFlat,
+    #TestSystShape,
+    RqcdStat,
+    RqcdSyst,
+    MuonEffStat,
+    MuonEffSys,
+    MuonEffTrigStat,
+    MuonEffTrigSys,
+    #MuonIsoStat,
+    #MuonIsoSys,
+    #MuonIdSys,
+    #MuonMsSys,
+    #MuonScaleSys,
+    BJetEigenB0,
+    BJetEigenB1,
+    #BJetEigenB2,
+    #BJetEigenB3,
+    #BJetEigenB4,
+    #BJetEigenC0,
+    #BJetEigenC1,
+    #BJetEigenC2,
+    #BJetEigenC3,
+    #BJetEigenLight0,
+    #BJetEigenLight1,
+    #BJetEigenLight2,
+    #BJetEigenLight3,
+    #BJetEigenLight4,
+    #BJetEigenLight5,
+    #BJetEigenLight6,
+    #BJetEigenLight7,
+    #BJetEigenLight8,
+    #BJetEigenLight9,
+    #BJetEigenLight10,
+    #BJetEigenLight11,
+    #BJetEigenLight12,
+    #BJetEigenLight13,
+    #BJetExtrapolation,
+]
 
 # Get computation environment
 cache = getattr(environment_file, 'persistent_cache', None)
@@ -188,6 +241,10 @@ if not exists(base_path):
 if arguments.root_output:
     root_file =  TFile.Open(join(base_path, 'tau_efficiencies.root'),
                             'RECREATE')
+
+# Open root file for output
+if arguments.text_output:
+    text_file =  open(join(base_path, 'tau_efficiencies_counts.txt'), 'w')
 
 print('Script options')
 print('  Output directory: {}'.format(base_path))
@@ -217,6 +274,10 @@ def do_efficiencies(distribution, region, efficiency_filter):
                 process, region.varied(efficiency_filter)
             )
         )
+        #print('{} total/passed: {:.1f}/{:.1f}'. \
+              #format(process.label(),
+                     #integral(backgrounds_total[-1]),
+                     #integral(backgrounds_passed[-1])))
 
     # Loop over signal samples and compute their histograms
     signals_total = []
@@ -236,35 +297,164 @@ def do_efficiencies(distribution, region, efficiency_filter):
             )
         )
 
+    systs_total_up = {}
+    systs_total_down = {}
+    systs_passed_up = {}
+    systs_passed_down = {}
+    for s in systematics:
+        name = s.name
+        systs_total_up[name] = []
+        systs_total_down[name] = []
+        systs_passed_up[name] = []
+        systs_passed_down[name] = []
+        for background in itervalues(backgrounds):
+            # Extract parameters
+            process = background['process']
+            estimation = background['estimation']
+            uncertainties = background['uncertainties']
+
+            nominal_total = estimation(distribution)(process, region)
+            nominal_passed = estimation(distribution)(
+                process, region.varied(efficiency_filter))
+
+            if s in uncertainties:
+                # Get the up/down of the total
+                _, _, up, down = to_shape(
+                    s(estimation(distribution))(process, region),
+                    nominal_total)
+                if up is not None and down is not None:
+                    systs_total_up[name].append(up)
+                    systs_total_down[name].append(down)
+                else:
+                    raise RuntimeError('something went wrong when applying '
+                                       'systematic variation ' + s.name)
+
+                # Get the up/down of the passed
+                _, _, up, down = to_shape(
+                    s(estimation(distribution))(
+                        process, region.varied(efficiency_filter)),
+                    nominal_passed)
+                if up is not None and down is not None:
+                    systs_passed_up[name].append(up)
+                    systs_passed_down[name].append(down)
+                else:
+                    raise RuntimeError('something went wrong when applying '
+                                       'systematic variation ' + s.name)
+            else:
+                #print('No systematic {} for {}. Using nominal.'. \
+                      #format(name, process.label()))
+                systs_total_up[name].append(nominal_total)
+                systs_total_down[name].append(nominal_total)
+                systs_passed_up[name].append(nominal_passed)
+                systs_passed_down[name].append(nominal_passed)
+
+            #print('{} up {} total/passed: {:.1f}/{:.1f}'. \
+                  #format('NOMINAL',
+                         #process.label(),
+                         #integral(nominal_total),
+                         #integral(nominal_passed)))
+            #print('{} up {} total/passed: {:.1f}/{:.1f}'. \
+                  #format(name,
+                         #process.label(),
+                         #integral(systs_total_up[name][-1]),
+                         #integral(systs_passed_up[name][-1])))
+            #print('{} down {} total/passed: {:.1f}/{:.1f}'. \
+                  #format(name,
+                         #process.label(),
+                         #integral(systs_total_down[name][-1]),
+                         #integral(systs_passed_down[name][-1])))
+
+
     # If capturing at pass 1, the result is bogus, continue
     if parallel.capturing():
-        return None,None
+        return None,None,None,None
 
-    # Create combined background histogram and subtract from
-    # data
-    if len(backgrounds_total) > 0:
-        background_total = add_histograms(backgrounds_total, 'MC')
-        background_passed = add_histograms(backgrounds_passed, 'MC')
-        data_total = data_total - background_total
-        data_passed = data_passed - background_passed
-
-
-    # Create combined signal histograms
+    ##############################################################
+    # COMPUTE SIGNAL EFFICIENCY
+    ##############################################################
     signal_total = add_histograms(signals_total, 'Signal')
     signal_passed = add_histograms(signals_passed, 'Signal')
 
-    ##############################################################
-    # COMPUTE EFFICIENCIES
-    ##############################################################
+    write_counts_signal(signal_total, signal_passed)
 
-    # Compute the efficiencies
-    data_efficiency = efficiency(data_total, data_passed)
     signal_efficiency = efficiency(signal_total, signal_passed)
-
-    data_efficiency.SetTitle('Data')
     signal_efficiency.SetTitle('MC')
 
-    return data_efficiency, signal_efficiency
+
+    ##############################################################
+    # COMPUTE NOMINAL DATA EFFICIENCY
+    ##############################################################
+    background_total = add_histograms(backgrounds_total, 'Bkg')
+    background_passed = add_histograms(backgrounds_passed, 'Bkg')
+    data_subtracted_total = data_total - background_total
+    data_subtracted_passed = data_passed - background_passed
+    data_efficiency = efficiency(data_subtracted_total, data_subtracted_passed)
+    data_efficiency.SetTitle('Data')
+
+    ##############################################################
+    # COMPUTE EFFICIENCIES FOR SYSTEMATIC VARIATIONS
+    ##############################################################
+    data_efficiency_up = []
+    data_efficiency_down = []
+    total_offset_up = []
+    total_offset_down = []
+    passed_offset_up = []
+    passed_offset_down = []
+    for s in systematics:
+        name = s.name
+        # NOTE: The up variation of the background removes a larger amount of
+        # the data, leaving a smaller remainder. Thus the up variation of the
+        # remainder is the data minus the down variation of the background,
+        # and vice versa.
+        syst_total_up = add_histograms(systs_total_up[name], 'Bkg')
+        syst_passed_up = add_histograms(systs_passed_up[name], 'Bkg')
+        data_subtracted_total_down = data_total - syst_total_up
+        data_subtracted_passed_down = data_passed - syst_passed_up
+
+        syst_total_down = add_histograms(systs_total_down[name], 'Bkg')
+        syst_passed_down = add_histograms(systs_passed_down[name], 'Bkg')
+        data_subtracted_total_up = data_total - syst_total_down
+        data_subtracted_passed_up = data_passed - syst_passed_down
+
+        total_offset_up.append(integral(data_subtracted_total_up) -
+                               integral(data_subtracted_total))
+        total_offset_down.append(integral(data_subtracted_total) -
+                                 integral(data_subtracted_total_down))
+        passed_offset_up.append(integral(data_subtracted_passed_up) -
+                               integral(data_subtracted_passed))
+        passed_offset_down.append(integral(data_subtracted_passed) -
+                                 integral(data_subtracted_passed_down))
+
+        write_counts_background(name, 'total',
+                                data_subtracted_total,
+                                data_subtracted_total_up,
+                                data_subtracted_total_down)
+        write_counts_background(name, 'passed',
+                                data_subtracted_passed,
+                                data_subtracted_passed_up,
+                                data_subtracted_passed_down)
+
+        # Compute the efficiencies
+        data_efficiency_up.append(efficiency(data_subtracted_total_up,
+                                             data_subtracted_passed_up))
+        data_efficiency_down.append(efficiency(data_subtracted_total_down,
+                                               data_subtracted_passed_down))
+
+        data_efficiency_up[-1].SetTitle(name + '_UP')
+        data_efficiency_down[-1].SetTitle(name + '_DOWN')
+
+    write_total_syst('total',
+                     integral(data_subtracted_total),
+                     sum_quadrature(total_offset_up),
+                     sum_quadrature(total_offset_down))
+    write_total_syst('total',
+                     integral(data_subtracted_passed),
+                     sum_quadrature(passed_offset_up),
+                     sum_quadrature(passed_offset_down))
+
+    return data_efficiency, signal_efficiency, \
+            data_efficiency_up, data_efficiency_down
+
 
 def plot_efficiencies(data_efficiency,
                       signal_efficiency,
@@ -318,6 +508,10 @@ def do_scale_factor(data_efficiency, signal_efficiency):
         except ZeroDivisionError:
             pass
 
+    if not sf_x:
+        raise RuntimeError('failed to create scale factors; '
+                           'scale factor array is empty')
+
     # Create the scale factor histogram (Data/MC) and fill it
     scale_factor = TGraphAsymmErrors(len(sf_x),
                                      array('d', sf_x),
@@ -353,11 +547,9 @@ def plot_scale_factor(scale_factor,
 
 def save_to_root(data_efficiency,
                  signal_efficiency,
+                 variation_efficiencies,
                  scale_factors,
                  file_name_components):
-    ##############################################################
-    # SAVE EFFICIENCIES AND SCALE FACTORS TO A ROOT FILE
-    ##############################################################
     root_file.cd()
 
     clone = data_efficiency.Clone(uuid4().hex)
@@ -370,10 +562,41 @@ def save_to_root(data_efficiency,
         '_'.join(['eff'] + file_name_components + ['mc']))
     clone.Write()
 
+    for eff in variation_efficiencies:
+        clone = eff.Clone(uuid4().hex)
+        clone.SetName(
+            '_'.join(['eff'] + file_name_components + [clone.GetTitle()]))
+        clone.Write()
+
     if scale_factors is not None:
         clone = scale_factors.Clone(uuid4().hex)
         clone.SetName('_'.join(['sf'] + file_name_components))
         clone.Write()
+
+def write_counts_signal(total, passed):
+    text_file.write('Signal total/passed: {:.1f}/{:.1f}\n'. \
+                    format(integral(total), integral(passed)))
+
+def write_total_syst(what, nominal_count, up_offset, down_offset):
+    text_file.write('{:30s} {:10s}: {:6.1f} ({:5.2f}%) | {:6.1f} | '
+                    '{:6.1f} ({:5.2f}%)\n'. \
+                    format('ALL',
+                           what,
+                           nominal_count - down_offset,
+                           -down_offset / nominal_count * 100.0,
+                           nominal_count,
+                           nominal_count + up_offset,
+                           up_offset / nominal_count * 100.0))
+
+def write_counts_background(syst, what, nominal, up, down):
+    nominal, up, down = integral(nominal), integral(up), integral(down)
+    def percentage(var):
+        return (var/nominal - 1.0) * 100.0
+    pc_up, pc_down = percentage(up), percentage(down)
+    text_file.write('{:30s} {:10s}: {:6.1f} ({:5.2f}%) | {:6.1f} | '
+                    '{:6.1f} ({:5.2f}%)\n'. \
+                    format(syst, what, down, pc_down, nominal, up, pc_up))
+
 
 # Run in a cached environment
 with caching_into(cache):
@@ -381,12 +604,15 @@ with caching_into(cache):
     # Run in a parallelized environment
     while parallel.run():
         if parallel.computed():
-            print('Creating plots...')
+            print('Computing efficiencies...')
 
         for eff_name, eff in iteritems(efficiencies):
             distribution = eff['distribution']
             efficiency_filter = eff['filter']
             region = eff['region']
+
+            if arguments.text_output and not parallel.capturing():
+                text_file.write('Efficiencies for {}\n'.format(eff_name))
 
             label = [region.label() + eff['title'], model['label']] + \
                     eff['label']
@@ -396,10 +622,11 @@ with caching_into(cache):
     ##############################################################
 
             # Compute and plot the efficiencies
-            data_efficiency, signal_efficiency = \
-                do_efficiencies(distribution,
-                                region,
-                                efficiency_filter)
+            data_efficiency, signal_efficiency, \
+                    data_efficiency_up, data_efficiency_down = \
+                    do_efficiencies(distribution,
+                                    region,
+                                    efficiency_filter)
 
             # If capturing at pass 1, the efficiencies are bogus, continue
             if parallel.capturing():
@@ -428,8 +655,12 @@ with caching_into(cache):
             if arguments.root_output:
                 save_to_root(data_efficiency,
                              signal_efficiency,
+                             data_efficiency_up + data_efficiency_down,
                              scale_factor,
                              [eff_name])
+
+if arguments.text_output:
+    text_file.close()
 
 if arguments.root_output:
     root_file.ls()
