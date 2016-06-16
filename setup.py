43b31071d650229037bb417c1e16d6fde57c14ed
Henrik Ohman <speeph@gmail.com>
2016-06-16 15:28:07 +0200

    Updated license and setup script

--- setup.py
+++ setup.py
@@ -6,33 +6,28 @@ from setuptools import setup, find_packages
 
 # owls-common imports
 path.append('common/modules')
-from version_check import owls_python_version_check
-
-
-# Check that this version of Python is supported
-owls_python_version_check()
 
 
 # Setup owls-cache
 setup(
     # Basic installation information
-    name = 'owls-taunu',
-    version = '0.0.1',
+    name = 'owls-mutau',
+    version = '0.0.2',
     packages = find_packages(exclude = ['common', 'testing']),
 
     # Dependencies
     install_requires = [
         'six >= 1.7.3',
-        'owls-cache >= 0.0.1',
-        'owls-parallel >= 0.0.1',
-        'owls-hep >= 0.0.1',
+        'owls-cache >= 0.0.2',
+        'owls-parallel >= 0.0.2',
+        'owls-hep >= 0.0.2',
     ],
 
     # Metadata for PyPI
     author = 'Henrik Öhman',
     author_email = 'speeph@gmail.com',
-    description = 'H+ to taunu analysis implementations',
+    description = 'ttbar -> mutau tag-and-probe analysis',
     license = 'MIT',
     keywords = 'python big data analysis',
-    url = 'https://github.com/spiiph/owls-taunu'
+    url = 'https://github.com/spiiph/owls-mutau'
 )
