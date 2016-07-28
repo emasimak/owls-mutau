0054940fbd075fe3361f88333dc0fdd13f7e9590
Henrik Ohman <speeph@gmail.com>
2016-07-28 23:35:14 +0200

    Added MC15B to publish plotting macro

--- scripts/plot-mutau-publish.sh
+++ scripts/plot-mutau-publish.sh
@@ -20,7 +20,7 @@ OSSS_REGIONS=" \
   #mu_tau \
   #mu_tau_tau25 \
   #"
-#OSSS_REGIONS="mu_tau"
+OSSS_REGIONS="mu_tau"
 
 OSSS_REGIONS_SYST=" \
   mu_tau \
@@ -32,14 +32,56 @@ DISTRIBUTIONS=" \
   tau_pt \
   tau_bdt_score_trig \
 "
+DISTRIBUTIONS="tau_pt"
 
 EXTENSIONS="pdf eps"
 EXTENSIONS="pdf"
 
-LUMINOSITY=3193.68 # 1/pb
-DATA_PREFIX="/disk/d2/ohman/lhtnp_v15_merged"
-YEAR=2015
+LUMINOSITY=3209.0 # 1/pb
+DATA_PREFIX="/disk/d1/ohman/tagprobe_2016-01-21_merged/"
 
+# Plots with OS-SS backgrounds
+OUTPUT="results/plots_publish/mc15b"
+"$OWLS/tools/plot.py" \
+  --output $OUTPUT \
+  --extensions $EXTENSIONS \
+  --model-file "$OWLS/definitions/models-2016-01-21.py" \
+  --model osss_fakes2 \
+  --regions-file "$OWLS/definitions/regions-2016-01-21.py" \
+  --regions $OSSS_REGIONS \
+  --distributions-file "$OWLS/definitions/distributions.py" \
+  --distributions $DISTRIBUTIONS \
+  --environment-file "$SCRIPTS/environment.py" \
+  --text-count \
+  --label "MC15B, 20.1" \
+  --error-label "Stat. Unc." \
+  data_prefix=$DATA_PREFIX \
+  enable_systematics=False \
+  luminosity=$LUMINOSITY
+
+## Plots with OS-SS backgrounds with systematic uncertainties
+#OUTPUT="results/plots_publish_syst/$YEAR"
+#"$OWLS/tools/plot.py" \
+  #--output $OUTPUT \
+  #--extensions $EXTENSIONS \
+  #--model-file "$OWLS/definitions/models-v12.py" \
+  #--model osss_fakes \
+  #--regions-file "$OWLS/definitions/models-v12.py" \
+  #--regions $OSSS_REGIONS_SYST \
+  #--distributions-file "$OWLS/definitions/distributions.py" \
+  #--distributions $DISTRIBUTIONS \
+  #--environment-file "$SCRIPTS/environment.py" \
+  #--text-count \
+  #--label "MC15C, 20.7" \
+  #--error-label "Stat. #oplus Sys. Unc." \
+  #data_prefix=$DATA_PREFIX \
+  #year=$YEAR \
+  #enable_systematics=True \
+  #luminosity=$LUMINOSITY
+
+LUMINOSITY=3193.68 # 1/pb
+DATA_PREFIX="/disk/d2/ohman/lhtnp_v16_merged"
+YEAR=2015
 
 # Plots with OS-SS backgrounds
 OUTPUT="results/plots_publish/$YEAR"
@@ -82,7 +124,7 @@ OUTPUT="results/plots_publish/$YEAR"
   #luminosity=$LUMINOSITY
 
 LUMINOSITY=7980.0 # 1/pb
-DATA_PREFIX="/disk/d2/ohman/lhtnp_v15_merged"
+DATA_PREFIX="/disk/d2/ohman/lhtnp_v16_merged"
 YEAR=2016
 
 # Plots with OS-SS backgrounds
